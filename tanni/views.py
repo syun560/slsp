889f68bec3211e725ba410901e531ac2b5f5096a
syun560 <al18056@shibaura-it.ac.jp>
2020-06-02 16:13:25 +0900

    README.MDファイルを追加

--- tanni/views.py
+++ tanni/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     params = {
-
+        'user': 'testuser',
     }
     return render(request, 'tanni/index.html', params)
