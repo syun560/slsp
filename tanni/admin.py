d572915c3e864600a66dc6c9ed094d4c36ee8f4c
syun560 <al18056@shibaura-it.ac.jp>
2020-06-16 13:49:06 +0900

    register admin site

--- tanni/admin.py
+++ tanni/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Subject, Course, ShibauraRule
 
 # Register your models here.
+admin.site.register(Subject)
+admin.site.register(Course)
+admin.site.register(ShibauraRule)
