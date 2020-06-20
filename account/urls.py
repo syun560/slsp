27376ddcf564622f3b13611715e27dbf312cb03f
syun560 <al18056@shibaura-it.ac.jp>
2020-06-20 21:37:38 +0900

    ログイン画面準備

---
+++ account/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+app_name = 'account'
+
+urlpatterns = [
+    path('login/', views.Login.as_view(), name='login'),
+    path('logout/', views.Logout.as_view(), name='logout'),
+    # path('send/', views.send_email), # メール送信テスト
+    
+    # path('user_create/', views.UserCreate.as_view(), name='user_create'),
+    # path('user_create/done', views.UserCreateDone.as_view(), name='user_create_done'),
+    # path('user_create/complete/<token>/', views.UserCreateComplete.as_view(), name='user_create_complete'),
+]
--- slsp/settings.py
+++ slsp/settings.py
@@ -122,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = 'account:login'
+LOGIN_REDIRECT_URL = 'home.index'
