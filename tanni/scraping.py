a1208d0271cc534c82869d1b0610fd9fd81dad64
KaitoAkz <al18003@shibaura-it.ac.jp>
2020-06-21 13:48:48 +0900

    add scraping.py

---
+++ tanni/scraping.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from urllib import request
+from bs4 import BeautifulSoup
+
+def scraping ( ):
+	#url
+	url = "http://syllabus.sic.shibaura-it.ac.jp/syllabus/2018/MatrixL01131.html.ja"
+
+	#get html
+	html = request.urlopen ( url )
+
+	#set BeautifulSoup
+	soup = BeautifulSoup ( html, "html.parser" )
+
+	#get ClassInfo
+	Subjects = soup.find_all ( "tr", attrs = { "class", "subject" } )
+	
+	#print ClassInfo
+	for subject in Subjects:
+		subject.td.decompose ( ) # tdタグを除去
+		print ( "講義名 : ", end = '' )
+		print ( subject.contents[0], subject.td.string )
+		subject.td.decompose ( ) # tdタグを除去
+		print ( "単位数 : ", end = '' )
+		print ( subject.contents[1], subject.td.string, "\n" )
+
+if __name__ == "__main__":
+	scraping ( )
