2a04b203ef1cecf9da3b7b47a3006d15aad0511c
KaitoAkz <al18003@shibaura-it.ac.jp>
2020-06-23 14:43:34 +0900

    scraping

--- tanni/scraping.py
+++ tanni/scraping.py
@@ -14,7 +14,32 @@ def scraping ( ):
 
 	#get ClassInfo
 	Subjects = soup.find_all ( "tr", attrs = { "class", "subject" } )
+
+	print ( "専門科目\n" )
 	
+	#print ClassInfo
+	for subject in Subjects:
+		subject.td.decompose ( ) # tdタグを除去
+		print ( "講義名 : ", end = '' )
+		print ( subject.contents[0], subject.td.string )
+		subject.td.decompose ( ) # tdタグを除去
+		print ( "単位数 : ", end = '' )
+		print ( subject.contents[1], subject.td.string, "\n" )
+
+	#url
+	url = "http://syllabus.sic.shibaura-it.ac.jp/syllabus/2018/MatrixL01161.html.ja"
+
+	#get html
+	html = request.urlopen ( url )
+
+	#set BeautifulSoup
+	soup = BeautifulSoup ( html, "html.parser" )
+
+	#get ClassInfo
+	Subjects = soup.find_all ( "tr", attrs = { "class", "subject" } )
+	
+	print ( "共通数理科目\n" )
+
 	#print ClassInfo
 	for subject in Subjects:
 		subject.td.decompose ( ) # tdタグを除去
