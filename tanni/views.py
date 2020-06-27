f5b52cae9a705009d6d1ab97797d258366ede90f
syun560 <al18056@shibaura-it.ac.jp>
2020-06-27 23:34:19 +0900

    時間割データを取得

--- tanni/views.py
+++ tanni/views.py
@@ -4,31 +4,36 @@ from django.contrib.auth.decorators import login_required
 from account.models import User
 from .models import UserTimeTable, Course
 
-@login_required
-def home(request):
+def getTimeTable(id):
     # ログインユーザの履修している科目を取得
-    time_table = UserTimeTable.objects.filter(user_id=request.user)
+    user_table = UserTimeTable.objects.filter(user_id=id)
 
     # 時間割に適した形で取得
-    jikanwari = [["" for s in range(6)] for ss in range(5)]
-    for tt in time_table:
-        jikanwari[tt.course_id.period - 1][tt.course_id.week - 1] = tt.course_id.subject_id.title
+    time_table = [["" for s in range(6)] for ss in range(5)]
+    for tt in user_table:
+        time_table[tt.course_id.period - 1][tt.course_id.week - 1] = tt.course_id.subject_id.title
 
+    return time_table
+
+@login_required
+def home(request):
     params = {
-        'jikanwari': jikanwari,
+        'time_table': getTimeTable(request.user),
     }
     return render(request, 'tanni/home.html', params)
 
 @login_required
-def userinfo(request):
+def userinfo(request, student_id):
+    # 学籍番号からUserを取得
+    selected_user = User.objects.filter(student_id=student_id).first()
     params = {
-        
+        'time_table': getTimeTable(selected_user.id),
+        'selected_user': selected_user,
     }
     return render(request, 'tanni/userinfo.html', params)
 
 @login_required
 def userlist(request):
-
     # すべてのユーザの情報を取得
     user_all = User.objects.all()
     params = {
@@ -38,8 +43,10 @@ def userlist(request):
 
 @login_required
 def reg(request):
+
+    
     params = {
-        
+        'time_table': getTimeTable(request.user),
     }
     return render(request, 'tanni/reg.html', params)
 
